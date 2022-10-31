937b91800e7913d6312dd59ba51f41936443d6e8
Ovik <47368700+Ovikx@users.noreply.github.com>
2022-10-31 01:00:22 -0500

    whatever

--- src/core/vector.rs
+++ src/core/vector.rs
@@ -1,3 +1,5 @@
+use std::ops::{Mul, Deref};
+
 pub enum VectorDType<T> {
     UInt(Vector<usize>),
     Int(Vector<isize>),
@@ -7,6 +9,13 @@ pub enum VectorDType<T> {
     Vector(Vector<T>)
 }
 
+pub enum Number {
+    UInt(usize),
+    Int(isize),
+    Float(f64)
+}
+
+
 #[derive(Debug)]
 pub struct Vector<T> {
     pub content: Vec<T>
@@ -15,8 +24,25 @@ pub struct Vector<T> {
 impl<T: Clone> Vector<T> {
     pub fn new(content: Option<&Vec<T>>) -> Self {
         match content {
-            Some(c) => Vector { content: c.clone() },
+            Some(c) => Vector { content: c.to_vec() },
             None => Vector { content: vec![] }
         }
+    }
+}
+
+impl Mul<f64> for Vector<Number> {
+    type Output = Vector<f64>;
+
+    fn mul(self, rhs: f64) -> Vector<f64> {
+        let mut res_vec: Vector<f64> = Vector { content: vec![] };
+        for item in self.content {
+            res_vec.content.push(match item {
+                Number::Float(n) => n * rhs,
+                Number::UInt(n) => n as f64 * rhs,
+                Number::Int(n) => n as f64 * rhs
+            });
+        }
+
+        res_vec
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 mod core;
 use crate::core::vector::Vector;
 fn main() {
-    let vec1 = vec![1,2,3,4];
+    let vec1 = (0..14).collect();
     let vector = Vector::new(Some(&vec1));
     println!("Vector: {:?}", &vector);
+    println!("Vector * 2.0 = {:?}", vector);
 }
