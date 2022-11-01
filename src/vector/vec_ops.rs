5614a51e8af2bd9188e2d8ea4e6810a2a20970b1
Ovikx <ovikdas10@gmail.com>
2022-11-01 11:21:13 -0500

    rename dot method to mag

--- src/vector/vec_ops.rs
+++ src/vector/vec_ops.rs
@@ -4,7 +4,7 @@ use crate::vector::base::Vector;
 use num::Float;
 
 impl<T: Float + AddAssign> Vector<T> {
-    pub fn dot(&self) -> T {
+    pub fn magnitude(&self) -> T {
         let mut total = T::zero();
         for n in &self.content {
             total += *n;
